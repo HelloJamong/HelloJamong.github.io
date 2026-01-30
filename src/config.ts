f79df141e5dccdbd7fb59facfd75977b1d25ae31
HelloJamong <hellowook@pm.me>
2026-01-30 13:46:05 +0900

    feat: 블로그 초기 구성

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://jaymong.me/",
+  author: "igor",
+  profile: "https://github.com/HelloJamong",
+  desc: "신선한 자몽 농장 - igor의 개발 블로그",
+  title: "신선한 자몽 농장",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 4,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: false,
+    text: "Edit page",
+    url: "https://github.com/HelloJamong/HelloJamong.github.io/edit/main/",
+  },
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  lang: "ko", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Seoul", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+} as const;
